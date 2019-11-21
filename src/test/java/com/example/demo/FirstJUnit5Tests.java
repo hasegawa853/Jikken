4bf0639843e23b5e8407f84709b2793ed8e1a9ab
lausol006 <lausol006@DESKTOP-TRDQPCC>
2019-11-21 10:13:13 +0900

    現在JUnit5を使っているのかJUnit4を使っているのか混乱中です。 countTest2()がどこを見ているのかわからない状況です。

--- src/test/java/com/example/demo/FirstJUnit5Tests.java
+++ src/test/java/com/example/demo/FirstJUnit5Tests.java
@@ -1,12 +1,17 @@
 package com.example.demo;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.login.domain.repository.UserDao;
 
 //テスト用アノテーション
 @RunWith(SpringRunner.class) // http://javatechnology.net/java/eclipse-junit5/参考hp
@@ -14,9 +19,25 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 class FirstJUnit5Tests {
 
+	@Autowired
+	@Qualifier("UserDaoJdbcImpl")
+	UserDao dao;
+
+	// カウントメソッドのテスト１
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void countTest1() {
+		// カウントメソッドの結果が2件であることをテスト
+		// staticインポートはEcripseで自動でつけてくれないため、注意する
+		assertEquals(dao.count(), 2);
+	}// 最初のメソッドは正常
+
+	// カウントメソッドのテスト２
+	// ポイント＠sql
+	@Test
+	@Sql("/testdata.sql")
+	public void countTest2() {// 実行結果が'script' must not be null or emptyのエラー
+		// カウントメソッドの結果が3件であることをテスト
+		assertEquals(dao.count(), 3);
 	}
 
 }
